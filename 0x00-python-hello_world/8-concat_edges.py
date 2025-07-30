1368e3e96777f2108f68f3d59cdfded0ff5f74ee
PitsoLephoi <pitsoalfredlephoi@gmail.com>
2025-07-30 13:29:21 +0200

    new file:   0x00-python-hello_world/6-concat.py 	new file:   0x00-python-hello_world/7-edges.py 	new file:   0x00-python-hello_world/8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR 
+print(str)
